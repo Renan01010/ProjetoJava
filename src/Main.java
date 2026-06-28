d0baf5fba49e574103de8f6871685e26ccafc9be
Renan Teles <renantelesdeoliveira@gmail.com>
2026-06-27 21:41:41 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Esse é op scream Match!");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+        String nomeFilme = "Top Gun: Maverick";
+
+    }
+}
